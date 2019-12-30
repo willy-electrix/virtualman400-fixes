1b683fa70869219e3e047113a9e3a87bde8c40d2
willy-electrix <59346441+willy-electrix@users.noreply.github.com>
2019-12-30 14:57:56 -0500

    correct paths

--- 02-clean-nes.sh
+++ 02-clean-nes.sh
@@ -6,9 +6,9 @@ DIR=$(cd `dirname $0` && pwd)
 cd $DIR
 
 mkdir 02-clean-nes
-mv "/home/pi/RetroPie/roms/nes/1" 02-clean-nes/
-mv "/home/pi/RetroPie/roms/nes/2" 02-clean-nes/
-mv "/home/pi/RetroPie/roms/nes/3" 02-clean-nes/
-mv "/home/pi/RetroPie/roms/nes/4" 02-clean-nes/
+mv "/home/pi/RetroPie/roms/nes/Battle City (J).nes" 02-clean-nes/
+mv /home/pi/RetroPie/roms/nes/samurai.zip 02-clean-nes/
+mv /home/pi/RetroPie/roms/nes/StreetFighterVI12.zip 02-clean-nes/
+mv /home/pi/RetroPie/roms/nes/tmnt2.zip 02-clean-nes/
 
 echo " fix 2 - done"
