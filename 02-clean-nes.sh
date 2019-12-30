979287a373b2790b75239697a09d5924ee854b4c
willy-electrix <willy-electrix@protonmail.com>
2019-12-30 08:47:35 +0000

    placeholders for the 4 files

--- 02-clean-nes.sh
+++ 02-clean-nes.sh
@@ -6,6 +6,9 @@ DIR=$(cd `dirname $0` && pwd)
 cd $DIR
 
 mkdir 02-clean-nes
-mv /home/pi/RetroPie/roms/nes/test.txt 02-clean-nes/
+mv "/home/pi/RetroPie/roms/nes/1" 02-clean-nes/
+mv "/home/pi/RetroPie/roms/nes/2" 02-clean-nes/
+mv "/home/pi/RetroPie/roms/nes/3" 02-clean-nes/
+mv "/home/pi/RetroPie/roms/nes/4" 02-clean-nes/
 
 echo " fix 2 - done"
