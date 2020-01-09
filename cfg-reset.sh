770514c7c34d451ee4a7fa03bb744eab9e984930
willy-electrix <willy-electrix@protonmail.com>
2020-01-09 00:51:46 +0000

    added .git

--- cfg-reset.sh
+++ cfg-reset.sh
@@ -8,7 +8,7 @@ fi
 cd /
 rm -rf .git
 git init
-git remote add origin https://github.com/willy-electrix/virtualman400-allconfigs
+git remote add origin https://github.com/willy-electrix/virtualman400-allconfigs.git
 git fetch --all
 git reset --hard origin/master
 rm -rf .git
